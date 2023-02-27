87d23229683f5fbd8f21349cdcc7b9e4220329b9
Мария Фалилеева <falileeva.ma@gmail.com>
2023-02-27 18:40:38 +0800

    update models

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Course, Chapter, Lesson, LessonItem
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sort')
+    fields = ('title',)
+    list_per_page = 40
+    empty_value_display = '-'
+    search_fields = ('title',)
+    sortable_by = ('title',)
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'sort')
+    fields = ('title', 'course')
+    list_per_page = 40
+    empty_value_display = '-'
+    list_filter = ('course',)
+    search_fields = ('title',)
+    sortable_by = ('title',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'chapter', 'sort')
+    fields = ('title', 'chapter')
+    list_per_page = 40
+    empty_value_display = '-'
+    list_filter = ('chapter',)
+    search_fields = ('title',)
+    sortable_by = ('title',)
+
+
+@admin.register(LessonItem)
+class LessonItemAdmin(admin.ModelAdmin):
+    list_display = ('lesson',)
+    fields = ('lesson', 'text')
+    list_per_page = 40
+    empty_value_display = '-'
+
+    search_fields = ('lesson',)
+    sortable_by = ('lesson',)
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,3 +1,59 @@
+from basis.model_mixin import BaseTimestampedModel
+from ckeditor.fields import RichTextField
 from django.db import models
 
-# Create your models here.
+
+class Course(BaseTimestampedModel):
+    title = models.CharField('Название', max_length=256)
+    sort = models.IntegerField(verbose_name='Сортировка', null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['sort']
+
+
+class Chapter(BaseTimestampedModel):
+    title = models.CharField('Название', max_length=256)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='course',
+        verbose_name='Курс',
+    )
+    sort = models.IntegerField(verbose_name='Сортировка', null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['sort']
+
+
+class Lesson(BaseTimestampedModel):
+    title = models.CharField('Название', max_length=256)
+    chapter = models.ForeignKey(
+        Chapter,
+        on_delete=models.CASCADE,
+        related_name='chapter',
+        verbose_name='Раздел',
+    )
+    sort = models.IntegerField(verbose_name='Сортировка', null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ['sort']
+
+
+class LessonItem(BaseTimestampedModel):
+    lesson = models.ForeignKey(Lesson, models.CASCADE, 'lesson_items', verbose_name='Урок')
+    text = RichTextField("Текст", default='', blank=True)
