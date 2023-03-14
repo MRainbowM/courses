fa00beb0a9290b466b8c707e0f7e1da26d88876a
Мария Фалилеева <falileeva.ma@gmail.com>
2023-03-14 19:33:56 +0800

    update urls

--- backend/basis/urls.py
+++ backend/basis/urls.py
@@ -1,12 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import include, path
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-
+    path('', include('frontend.urls')),
     path('courses/', include('courses.urls')),
     path('admin/', admin.site.urls),
 ]
@@ -19,3 +17,7 @@ urlpatterns += [
     ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
