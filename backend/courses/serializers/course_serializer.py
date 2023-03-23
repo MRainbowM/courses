74b38f471d568086ef05934303471a078ab9043d
Мария Фалилеева <falileeva.ma@gmail.com>
2023-03-23 15:18:47 +0800

    update CourseReadOnlySerializer

--- backend/courses/serializers/course_serializer.py
+++ backend/courses/serializers/course_serializer.py
@@ -1,10 +1,21 @@
 from rest_framework.serializers import (
     Serializer,
     IntegerField,
-    CharField
+    CharField,
+    SerializerMethodField
 )
+
+from courses.models import Chapter
 
 
 class CourseReadOnlySerializer(Serializer):
     id = IntegerField(read_only=True)
     title = CharField(read_only=True)
+
+    chapters = SerializerMethodField(read_only=True)
+
+    def get_chapters(self, obj) -> list:
+        сhapters = Chapter.objects.filter(course_id=obj.id).order_by('sort').values(
+            'id', 'title'
+        )
+        return сhapters
