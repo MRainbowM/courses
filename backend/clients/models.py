2fb1537ae4cd1eb5ed88794933dabae5d8f37390
Мария Фалилеева <falileeva.ma@gmail.com>
2023-04-28 14:56:54 +0800

    create clients

---
+++ backend/clients/models.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from courses.models import Course
+
+
+class Client(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_client',
+        verbose_name='Пользователь',
+        null=True,
+        blank=True,
+    )
+
+    active = models.BooleanField(
+        verbose_name='Активность',
+        help_text='Включено - участвует в проекте, выключено - нет',
+        default=True,
+    )
+
+    class Meta:
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+
+class ClientCourse(models.Model):
+    """Курсы участника"""
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        related_name='client_course',
+        verbose_name='Клиент'
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='client_course',
+        verbose_name='Курс'
+    )
+    active = models.BooleanField(
+        verbose_name='Активность',
+        help_text='Включено - есть доступ к курсу, выключено - нет',
+        default=False,
+    )
+
+
+class ClientLesson(models.Model):
+    """Пройденные уроки участника"""
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        related_name='client_lesson',
+        verbose_name='Клиент'
+    )
+    lesson = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='client_lesson',
+        verbose_name='Урок'
+    )
+    completed = models.BooleanField(
+        verbose_name='Выполнен',
+        help_text='Включено - урок пройден, выключено - нет',
+        default=False,
+    )
