3909bcb3277d60d0c3c31325f7c3a114276ea066
Мария Фалилеева <falileeva.ma@gmail.com>
2023-04-28 16:52:42 +0800

    update RegisterUserView

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,18 +1,22 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from clients.models import Client
 from users.forms import UserRegisterForm
 
 
 class RegisterUserView(CreateView):
     form_class = UserRegisterForm
     template_name = 'register.html'
-    success_url = reverse_lazy('login')
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     c_def = self.get_user_context(title='Регистрация')
+    # success_url = reverse_lazy('login')
+    success_url = '/'
 
     def post(self, request, *args, **kwargs):
-        self.object = None
-        return super().post(request, *args, **kwargs)
+        answer = super().post(request, *args, **kwargs)
+
+        if self.object is not None:
+            # Пользователь успешно создан
+            # -> создание клиента
+            Client.objects.create(user=self.object, active=True)
+
+        return answer
